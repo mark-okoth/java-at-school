995eb124f29aff1c0222649283df9161694d6257
mark odhiambo okoth <markokoth96@gmail.com>
2020-02-26 19:04:00 +0300

    working with strings

--- edu.java
+++ edu.java
@@ -1,7 +1,11 @@
 public class edu{
     public static void main(String[]  args) {
         String name = "michael oliver";
+
+        System.out.println(name.indexOf('o'));
         System.out.println(name.length());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
         
     }
 }
