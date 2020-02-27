c3f50ac0e0142956dc5770fd2e5acf9778a73fe5
mark odhiambo okoth <markokoth96@gmail.com>
2020-02-27 18:33:02 +0300

    moving on

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,6 @@
 public class HelloWorld{
     public static void main(String[] args){
         System.out.print("hello world we made it");
-        int mark = 450;  
-        if(mark > 20){
-            System.out.println("it is too small for me");
-        }else{
-            System.out.print("it is large  for me"); 
-        }
+      
     }
 }
