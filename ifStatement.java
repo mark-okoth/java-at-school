775ca1eff1c80355e1499a9d00205ec429a5629f
mark odhiambo okoth <markokoth96@gmail.com>
2020-02-27 18:46:35 +0300

    if statement in java

---
+++ ifStatement.java
@@ -0,0 +1,13 @@
+public class ifStatement {
+    public static void main(String[] args){
+    int mark = 20;
+    if(mark > 20){
+      System.out.println("the number is too big");
+    } else if(mark == 20) {
+        System.out.println("The number is equal to 20");
+    }else{
+        System.out.println("The number is less than 20");
+    }
+
+    }
+}
